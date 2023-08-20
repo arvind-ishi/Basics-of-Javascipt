e866da7fa7691d35ec22b99e102dbd6d73949c71
arvind jadhav <126157696+arvind-ishi@users.noreply.github.com>
2023-08-20 10:28:45 +0000

    this is the new code

--- avi/avi.js
+++ avi/avi.js
@@ -1,7 +1,24 @@
 console.log("shivay");
 console.log("arvind");
 console.log("avi");
+let sa = 12;
+const ar = 12;
 
 let avi = 12;
 
-console.log (typeof avi)
+console.log (typeof avi);
+
+console.log("hello world");
+
+let sontosh = "go";
+
+console.table({sontosh, avi, ar,sa});
+// 
+
+
+// in the not use the var in the js file new vesion for 
+
+
+
+/// second datatypes and variables********
+
