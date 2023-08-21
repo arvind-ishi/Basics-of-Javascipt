376de09e2fb1011f24423276b0742fcb0ba19b82
arvind jadhav <126157696+arvind-ishi@users.noreply.github.com>
2023-08-21 08:14:09 +0000

    conversion datatypes

---
+++ avi/datatype.js
@@ -0,0 +1,232 @@
+//conversion datatypes learn 
+
+
+// let score = "33";
+
+// console.log(typeof score);
+// console.log(typeof score );
+
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+///out put 
+
+//string 
+//string
+//number 
+// 33
+
+
+
+// let score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof score );
+
+
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+
+//out put 
+
+//string 
+//string 
+//number 
+// NaN
+// let score = "hitesh";
+
+// console.log(typeof score);
+// console.log(typeof score );
+
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+///out puut
+//number 
+
+//nan
+
+
+// let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof score );
+
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+
+//output
+//undefined
+//undefined
+//number 
+//nan
+
+// let score = true;
+
+// console.log(typeof score);
+// console.log(typeof score );
+
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+
+//output
+
+//boolean
+//boolean
+//number
+//1
+
+// let score = false;
+
+// console.log(typeof score);
+// console.log(typeof score );
+
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+//output 
+
+//boolean
+//boolean
+//number
+
+//0
+
+
+
+//****notes of the  */
+
+// "33" => 33
+//  "33abc0" => NaN
+
+//true = 1; flase = 0
+
+// undefinde = > NaN
+
+
+// let inboolean = 1;
+// let valueBoolean = Boolean(inboolean);
+
+// console.log(valueBoolean);
+
+// true out put 
+
+// let inboolean = " ";
+// let valueBoolean = Boolean(inboolean);
+
+// console.log(valueBoolean);
+
+
+// true " ";
+
+//but 
+
+// let inboolean = "";
+// let valueBoolean = Boolean(inboolean);
+
+// console.log(valueBoolean);
+
+// false = "";
+
+// let inboolean = " hetesh";
+// let valueBoolean = Boolean(inboolean);
+
+// console.log(valueBoolean);
+
+// true = > " hetesh"
+
+
+//*** note all  */
+
+/// true = 1; 
+// false = 0 ;
+// "" => false ; 
+//"hetesh " true;
+
+
+
+// let someNumber = 33;
+
+// let stringNumber = String(someNumber);
+
+// console.log(stringNumber);
+// console.log( typeof stringNumber);
+
+
+
+
+//**************** operations******************* */
+
+// let avi = 3;
+
+// console.log(-avi);
+
+//output = -3 
+
+// let num = 3;
+// let Number = -num;
+// console.log(Number);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**3);
+// console.log(2&2);
+// console.log(2!=2);
+// console.log(2|2);
+
+let str1 = "hetesh";
+
+let str2 = " arvind";
+
+let str3 = str1 +str2;
+
+console.log(str3);
+
+
+console.log("1"+2);//12
+console.log(1 +"2");//12
+console.log( "1"+"2");//12
+console.log( "1" +2+2);//122
+console.log(1 + 2+ " 2");//23
+
+
+console.log( +true);
+console.log(+"");
+
+let num1 ,num2, num3;
+
+num1 = num2 = num3 = 2+2;
+
+console.table({num1, num2,num3})
+
+let gameCounter = 100;
+
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
---
+++ avi/ini.js
@@ -0,0 +1,37 @@
+///datatypes and variuabes
+
+"use strict";
+
+
+console.log(3+
+    3)
+    ///code readability will be high 
+
+
+    let name = "avish"
+    let number = 12
+    let age = 18 
+    let isLoggedIn = true
+
+    let state ;
+
+
+    /// number = 2 to the power 53
+
+    // 1) bigit
+
+    //2) string => ""
+    // 3) null => standloan value 
+    //4) boolean = > true /false 
+
+    //5) undefinid symbol =>unique
+
+
+    // 6) objects
+
+
+    // this is the type any operator in the content 
+
+    console.log(typeof undefined);
+    console.log(typeof null);
+    
