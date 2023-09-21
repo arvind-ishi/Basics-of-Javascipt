4062b86398c7aa1365603a194312f5b3d4e7f80e
arvind jadhav <126157696+arvind-ishi@users.noreply.github.com>
2023-09-21 07:39:49 +0530

    the objects in destructuring

---
+++ destructuring.js
@@ -0,0 +1,15 @@
+const corse = {
+    corsename : " hindi ans code ",
+    price : "999",
+    corseIntructure : "arvind"
+
+}
+
+//console.log(corse.corseIntructure);
+////////destructuring ==objects 
+
+const {corseIntructure : Intructure} = corse ;
+
+console.log(Intructure);
+
+
